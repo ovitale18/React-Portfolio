b5b7e8debabfc152c94584c568fe56b049f88308
Oliver Vitale <ovitale18@gmail.com>
2023-03-28 13:37:29 -0400

    project page cards working

---
+++ my-app/src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/ddbb.png";
+import pro2 from "../assets/everybody-eats.png";
+import pro3 from "../assets/placeholder.jpg";
+
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Digital DIY Bulletin Board",
+        text: "In this project me and my team created a program where you can create an account with a login and password, and create posts to promote upcoming public events.",
+        view: "https://digital-diy-bulletin-board.herokuapp.com/"
+    },
+
+    {
+        imgsrc: pro2,
+        title: "Everybody Eats",
+        text: "Do you ever find yourself hungry and wanting to try something new but not sure where to start? With the Everybody Eats app you can select a type of food and region and have a meal idea with a recipe in seconds.",
+        view: "https://kpfranklin22.github.io/Everybody-Eats/#"
+    },
+
+    {
+        imgsrc: pro3,
+        title: "Coming soon...",
+        text: "",
+        view: "https://github.com/ovitale18"
+    },
+];
+
+export default ProjectCardData;
--- my-app/src/routes/About.js
+++ my-app/src/routes/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <Navbar />
+      <HeroImg2 heading="ABOUT." text="I am a Full Stack Developer."/>
+      <Footer />
+    </div>
+  );
+};
 
-export default About
+export default About;
